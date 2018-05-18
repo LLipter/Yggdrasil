e8b546b9fafb92ac8ef4298c34659aaf303171d8
Mihato <996459164@qq.com>
2018-05-18 23:32:15 +0800

    add Global.cs

---
+++ Yggdrasil/Model/Global.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yggdrasil.Model
+{
+    class Global
+    {
+        private static User user;   // represents current user
+    }
+}
