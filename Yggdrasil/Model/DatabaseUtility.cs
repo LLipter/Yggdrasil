1973dcc09ad86a3ae99a37e0468baf2d12e9957f
Mihato <996459164@qq.com>
2018-05-18 17:59:56 +0800

    add User class and DatabaseUtility class

---
+++ Yggdrasil/Model/DatabaseUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yggdrasil.Model
+{
+    class DatabaseUtility
+    {
+        public static User getUser(String userName)
+        {
+            return null;
+        }
+    }
+}
