c27923e17cc9c8c7d0915b0c1ba3838a7bf6c8bf
410954231 <410954231@qq.com>
2018-06-01 18:18:21 +0800

    change of search

--- Yggdrasil/Search.cs
+++ Yggdrasil/Search.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,172 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Yggdrasil.Model;
 
 namespace Yggdrasil
 {
     public partial class Search : Form
     {
+        static int pageNum = 0;
+        static int totalPage = 0;
+        private int size;
+        private string tempSite = "http://www.irran.top:8080/Yggdrasil/book/";
+        private ArrayList bookList = new ArrayList();
+        private ArrayList labelList = new ArrayList();
+        private ArrayList imageList = new ArrayList();
+
+        
+
         public Search()
         {
             InitializeComponent();
+            labelList.Add(book1);
+            labelList.Add(book2);
+            labelList.Add(book3);
+            labelList.Add(book4);
+            labelList.Add(book5);
+            labelList.Add(book6);
+            imageList.Add(image1);
+            imageList.Add(image2);
+            imageList.Add(image3);
+            imageList.Add(image4);
+            imageList.Add(image5);
+            imageList.Add(image6);
+        }
+
+        private void Search_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            pageNum = 0;
+            clear();
+            if(searchText.Text == "")
+            {
+                MessageBox.Show("Please enter the name of book");
+            }
+            else
+            {
+                byte[] utf8 = Encoding.UTF8.GetBytes(searchText.Text.ToString());
+                string wantBook = Encoding.UTF8.GetString(utf8);
+                if (getData(wantBook) == 1)
+                {
+                    showBook();
+                }
+            }
+        }
+
+        private void changePageButton_Click(object sender, EventArgs e)
+        {
+            if (pageChangeText.Text == "")
+            {
+                MessageBox.Show("Please enter the page number!");
+            }
+            else
+            {
+                pageNum = Convert.ToInt32(pageChangeText.ToString());
+                showBook();
+            }
+        }
+
+        private void lastPageButton_Click(object sender, EventArgs e)
+        {
+            if(pageNum == 1)
+            {
+                MessageBox.Show("This is the first page!");
+            }
+            else
+            {
+                pageNum--;
+                showBook();
+            }
+        }
+
+        private void nextPageButton_Click(object sender, EventArgs e)
+        {
+            if (pageNum == totalPage)
+            {
+                MessageBox.Show("This is the Last page!");
+            }
+            else
+            {
+                pageNum++;
+                showBook();
+            }
+        }
+
+        private int getData(String name)
+        {
+            if (DatabaseUtility.getBookByName(ref bookList, name) == -1)
+            {
+                MessageBox.Show("Error, the network doesn't connect!");
+                return -1;
+            }
+            else
+            {
+                size = bookList.Count;
+                if(size == 0)
+                {
+                    MessageBox.Show("Sorry, there isn't such book!");
+                    return 0;
+                }
+                if (size % 6 == 0)
+                {
+                    totalPage = size / 6;
+                }
+                else
+                {
+                    totalPage = (size / 6) + 1;
+                }
+                return 1;
+            }
+        }
+
+        private void showBook()
+        {
+            Book book = new Book();
+            Label labelShow = new Label();
+            PictureBox imageShow = new PictureBox();
+            if (pageNum == 0)
+            {
+                pageNum++;
+            }
+            for (int i = 6 * pageNum + 1; i < size; i++)
+            {
+                string location;
+                string targetSite;
+                int temp = i % 6;
+                if(temp == 7)
+                {
+                    return;
+                }
+                labelShow = (Label)labelList[temp - 1];
+                imageShow = (PictureBox)imageList[temp - 1];
+                book = (Book)bookList[temp - 1];
+                labelShow.Text = book.Book_name;
+                location = book.Location;
+                targetSite = tempSite + location + "/cover.jpg";
+                imageShow.Load(targetSite);
+            }
+        }
+
+        private void clear()
+        {
+            Label label;
+            PictureBox image;
+
+            for (int i = 0; i < 6; i++)
+            {
+                label = (Label)labelList[i];
+                label.Text = "";
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                image = (PictureBox)imageList[i];
+                image.Image = null;
+            }
         }
     }
 }
